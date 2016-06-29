b8bd7af3853d2cac2525c672fee315d38d19cb6f
zyjxs <zyjxs_happy@sina.com>
2016-06-29 14:46:39 +0800

    Initial Commit

---
+++ BannerScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BannerScrollView
+//
+//  Created by zyj on 16/6/29.
+//  Copyright © 2016年 UIView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BannerScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BannerScrollView
+//
+//  Created by zyj on 16/6/29.
+//  Copyright © 2016年 UIView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
